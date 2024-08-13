869ec9b0a5a7205b380251d1aecb6adc7164eb5f
Matts52 <32623708+Matts52@users.noreply.github.com>
2024-08-13 00:11:03 -0600

    update

--- integration_tests/models/test_rare_category_encode.sql
+++ integration_tests/models/test_rare_category_encode.sql
@@ -4,6 +4,5 @@ with data_rare_category_encode as (
 
 select
     {{ dbt_ml_inline_preprocessing.rare_category_encode('input', cutoff=0.20) }} as actual_20,
-    --(COUNT(*) over (partition by input) / (COUNT(*) over ())::FLOAT) as test,
     output_20 as expected_20
 from data_rare_category_encode
