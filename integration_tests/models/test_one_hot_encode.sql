75a46c733591db148bfd98741bad1272258eceab
Matts52 <32623708+Matts52@users.noreply.github.com>
2025-03-21 00:01:14 -0600

    update one hot encode to accept explicit categories

--- integration_tests/models/test_one_hot_encode.sql
+++ integration_tests/models/test_one_hot_encode.sql
@@ -3,7 +3,12 @@ with data_one_hot_encode as (
 )
 
 select
-    {{ dbt_ml_inline_preprocessing.one_hot_encode('input', ref('data_one_hot_encode')) }},
+    {{
+        dbt_ml_inline_preprocessing.one_hot_encode(
+            column='input',
+            source_relation=ref('data_one_hot_encode')
+        )
+    }},
     output_is_input__cat as expected_is_input__cat,
     output_is_input__dog as expected_is_input__dog,
     output_is_input__ as expected_is_input__
