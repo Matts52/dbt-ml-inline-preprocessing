9446d7947e2b79e9b3ffa2676a5964e4b552db8c
Matts52 <32623708+Matts52@users.noreply.github.com>
2025-03-22 18:00:16 -0600

    update closeness test

--- integration_tests/macros/tests.sql
+++ integration_tests/macros/tests.sql
@@ -14,7 +14,7 @@
 {% endtest %}
 
 {% test assert_close(model, actual, expected, decimal_place=2) %}
-    select * from {{ model }} where round({{ actual }}::numeric, 2) != round({{ expected }}::numeric, 2)
+    select * from {{ model }} where round({{ actual }}::numeric, {{ decimal_place}}) != round({{ expected }}::numeric, {{ decimal_place }})
 {% endtest %}
 
 {% test assert_not_null(model, column) %}
