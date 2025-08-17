680753b48b3a622f94ccf8de82f8498a5e70953f
Matts52 <32623708+Matts52@users.noreply.github.com>
2025-08-16 21:33:55 -0600

    updates

--- macros/cyclic_encode.sql
+++ macros/cyclic_encode.sql
@@ -8,17 +8,19 @@
         {% set expr = "extract(hour from " ~ column ~ ")" %}
         {% set max_val = 24 %}
     {% elif period == "day_of_week" %}
-        {% set expr = "extract(dow from " ~ column ~ ")" %}
+        {# Adjusting for function returning 0-6 range #}
+        {% set expr = "(extract(dow from " ~ column ~ ") + 1)" %}
         {% set max_val = 7 %}
     {% elif period == "day_of_month" %}
         {% set expr = "extract(day from " ~ column ~ ")" %}
+        {# Note, this does not adjust for different months having a different number of days #}
         {% set max_val = 31 %}
     {% elif period == "month_of_year" %}
         {% set expr = "extract(month from " ~ column ~ ")" %}
         {% set max_val = 12 %}
     {% elif period == "week_of_year" %}
         {% set expr = "extract(week from " ~ column ~ ")" %}
-        {% set max_val = 52 %}
+        {% set max_val = 53 %}
     {% else %}
         {{ exceptions.raise_compiler_error(
             "Unsupported period type: " ~ period ~ ". Supported period types are: hour_of_day, day_of_week, day_of_month, month_of_year, week_of_year."
